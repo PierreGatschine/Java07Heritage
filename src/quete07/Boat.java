fc4492f90d86fea16eaef119c03e9b84f979f6e5
Pierre Gatschiné <pierre.gatschine@gmail.com>
2019-09-20 05:52:36 +0200

    Bon pour correctioon

---
+++ src/quete07/Boat.java
@@ -0,0 +1,13 @@
+package quete07;
+
+public class Boat extends Vehicle {
+
+	public Boat(String brand, int kilometers) {
+		super(brand, kilometers);
+	}
+
+	public String doStuff() {
+		return " Je suis " + this.getBrand() + " et je fais glou glou !";
+	}
+
+}
---
+++ src/quete07/Hangar.java
@@ -0,0 +1,18 @@
+package quete07;
+
+public class Hangar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Car Chausson = new Car("Henri", 90);
+		Boat Beneteau = new Boat("Jeannette", 1000);
+		
+		System.out.println(Chausson.doStuff());
+	    System.out.println(Beneteau.doStuff());
+		
+	}
+	
+	
+
+}
